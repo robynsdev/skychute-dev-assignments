6e59000db2c6b0a6f268a19b29f88bb07c2258af
robyn <-h>
2021-02-25 19:02:27 +1100

    assignment#2 task 3 added psuedocode comments

--- src/assignment_02/task3.ts
+++ src/assignment_02/task3.ts
@@ -1,8 +1,21 @@
 function maxPossibleNum(n: number, k: number): number {
+  // seperate given decimal number (n) into 2 strings:
+  // (before-decimal) str[0] & (after-decimal) str[1]
   const str: string[] = String(n).split(".");
   let stack: string[] | null = null;
   let digits: number = k;
   let maximum: number = 0;
+
+  // if (after-decimal) is greater than (digits)
+  // 1. run removeDigitsHelper function for (after-decimal)
+  // 2. concat (before-decimal), ".", (stack) then change to number for (maximum)
+  // else
+  // 1. add the count of zeroes in (after-decimal) and add to (digits),
+  // deduct the length of (after-decimal) from (digits)
+  // 2. run removeDigitsHelper function for (before-decimal)
+  // 3. change (stack) to number for (maximum)
+  //
+  // return the (maximum)
 
   if (str[1].length > digits) {
     stack = removeDigitsHelper(str[1], digits);
@@ -18,6 +31,15 @@ function maxPossibleNum(n: number, k: number): number {
 
 function removeDigitsHelper(str: string, digits: number): string[] {
   let stack: string[] = [];
+
+  // iterate through (str)
+  // when 3 conditions met, (stack.pop) and decrease (digits):
+  // 1) (stack) is not empty &
+  // 2) (digits) to remove is > 0 &
+  // 3) current char in (str) is greater than the last char in (stack)
+  // else add the current char to (stack)
+  //
+  // return the (stack) when iteration is complete
 
   for (let c of str) {
     while (
